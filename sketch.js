d7cf0e0c349e8a35b73b3f2d58beaeaabbe5e89b
Tanvi-2230 <77377638+Tanvi-2230@users.noreply.github.com>
2021-05-10 15:46:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, paper;
+var left, middle, right;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(width/2, height-40, width, 20);
+
+	paper = new Paper(600, height-60, 15);
+
+	left = new Dustbin(950, height-100, 20, 100);
+  right = new Dustbin(800, height-100, 20, 100);
+	middle = new Dustbin(880, height-60,  150, 20);
+	
+
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  
+  ground.display();
+
+  paper.display();
+
+  left.display();
+  middle.display();
+  right.display();
+  
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+   if(keyCode === UP_ARROW){
+	   Matter.Body.applyForce(paper.body, paper.body.position, {x:25, y: -27})
+   }
+
+
+}
+
